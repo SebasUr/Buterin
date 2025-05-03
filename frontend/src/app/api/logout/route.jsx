b07a502bc74b33ae026f7728c68f2a58815279c6
Daniel Santana Meza <91964922+Tazana2@users.noreply.github.com>
2025-05-03 16:34:36 -0500

    Revert "Refactor/frontend improvement"

--- frontend/src/app/api/logout/route.jsx
+++
@@ -1,7 +0,0 @@
-import { removeAllTokens } from "@/lib/auth";
-import { NextResponse } from "next/server";
-
-export async function POST(params) {
-  const tal = await removeAllTokens()
-  return NextResponse.json({}, {status: 200})
-}
